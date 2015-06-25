3f749ceee8c62c16ae37df975747fd107469afc1
James Brill <jebssnooker@hotmail.com>
2015-06-25 21:09:38 +0100

    Make dummy state cycle in main controller

--- static/scripts/controller.js
+++ static/scripts/controller.js
@@ -1,9 +1,41 @@
 function Controller() 
 {
-	flashcardController = new FlashcardController();
+	this.states = 
+	[
+		"ConfigForm",
+		"DelegateToFlashcardController",
+		"SessionComplete"
+	];
+	this.stateIndex = 0;
+	flashcardView = new FlashcardView();
 }
 
-Controller.prototype.performPhase = function()
+Controller.prototype.nextState = function()
 {
-	this.flashcardController.performPhase();
+	var numberOfStates = this.states.length;
+	this.stateIndex = (this.stateIndex + 1) % numberOfStates;
+	this.processState();
+}
+
+Controller.prototype.processState = function()
+{
+	var state = this.states[this.stateIndex];
+	switch (state)
+	{
+		case "ConfigForm":
+			console.log("A");
+			this.nextState();
+			break;
+		case "DelegateToFlashcardController":
+			flashcardController = new FlashcardController();
+			flashcardController.performPhase();
+			break;
+		case "SessionComplete":
+			console.log("B");
+			flashcardView.clear();
+			this.nextState();
+			break;
+		default: 
+			alert("Invalid state.");
+	}
 }
