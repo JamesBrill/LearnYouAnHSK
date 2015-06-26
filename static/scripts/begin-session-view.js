8fdac329839a69321562552a611ba6cd04fce59d
James Brill <jebssnooker@hotmail.com>
2015-06-26 23:35:44 +0100

    Add dummy radio button set for test type

--- static/scripts/begin-session-view.js
+++ static/scripts/begin-session-view.js
@@ -1,12 +1,20 @@
 function BeginSessionView() 
 {
 	this.radioButtonSize = TEXT_SIZE;
-	this.radioButtonSetSize = 3 * this.radioButtonSize;
-	this.hskRadioButtons = canvas.drawRadioButtons(0.5 * (canvas.width - this.radioButtonSetSize), 
+	this.hskRadioButtonSetSize = 3 * this.radioButtonSize;
+	this.hskRadioButtons = canvas.drawRadioButtons(0.5 * (canvas.width - this.hskRadioButtonSetSize), 
 							0.1 * canvas.height, 
 							"blue", 
 							[["HSK 1"], ["HSK 2"]], 
 							[function() { controller.setHSKWordList(1); controller.nextState(); }, function() { controller.setHSKWordList(2); controller.nextState(); }], 
+							this.radioButtonSize);
+
+	this.testTypeRadioButtonSetSize = 7 * this.radioButtonSize;
+	this.testTypeRadioButtons = canvas.drawRadioButtons(0.5 * (canvas.width - this.testTypeRadioButtonSetSize), 
+							0.1 * canvas.height + TEXT_SIZE, 
+							"green", 
+							[["Characters +", "Pinyin"], ["Characters"], ["Pinyin"], ["English", "Translation"]], 
+							[function() {  }, function() {  }, function() {  }, function() {  }], 
 							this.radioButtonSize);
 }
 
