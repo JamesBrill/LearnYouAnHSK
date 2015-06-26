34d2bb09e2b41492c0a3d9b73d0ce9abb79a8f3c
James Brill <jebssnooker@hotmail.com>
2015-06-26 23:08:49 +0100

    Centre radio buttons

--- static/scripts/begin-session-view.js
+++ static/scripts/begin-session-view.js
@@ -1,11 +1,13 @@
 function BeginSessionView() 
 {
-	this.radioButtons = canvas.drawRadioButtons(100, 
+	this.radioButtonSize = 100;
+	this.radioButtonSetSize = 5 * this.radioButtonSize;
+	this.radioButtons = canvas.drawRadioButtons(0.5 * canvas.width - 0.4 * this.radioButtonSetSize, 
 							100, 
 							"red", 
 							["A", "B", "C"], 
 							[function(){controller.nextState()}, function(){controller.nextState()}, function(){controller.nextState()}], 
-							100);
+							this.radioButtonSize);
 }
 
 BeginSessionView.prototype.resetRadioButtons = function()
