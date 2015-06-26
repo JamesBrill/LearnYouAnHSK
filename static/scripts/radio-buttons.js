90bf2329dbfb9615b01a716bba0864520735fff7
James Brill <jebssnooker@hotmail.com>
2015-06-26 22:50:40 +0100

    Add radio buttons class

---
+++ static/scripts/radio-buttons.js
@@ -0,0 +1,33 @@
+function RadioButtons(strokeSize) 
+{
+	this.buttons = [];
+	this.strokeSize = strokeSize;
+}
+
+RadioButtons.prototype.select = function(index)
+{
+	for (var i = 0; i < this.buttons.length; i++)
+	{
+		if (i == index)
+		{
+			this.buttons[i].box.attr({ stroke: "yellow", "stroke-width": this.strokeSize });			
+		}
+		else
+		{
+			this.buttons[i].box.attr({ stroke: null });
+		}
+	}
+}
+
+RadioButtons.prototype.reset = function()
+{
+	for (var i = 0; i < this.buttons.length; i++) 
+	{
+		var box = this.buttons[i].box;
+		var stroke = box.attr("stroke");
+		if (stroke == "white")
+		{
+			box.attr("stroke", null);
+		}
+	}
+}
