a72e827ec8f11333eea3dc3045a0320699af6a36
James Brill <jebssnooker@hotmail.com>
2015-06-26 23:16:35 +0100

    Add dummy HSK radio buttons

--- static/scripts/begin-session-view.js
+++ static/scripts/begin-session-view.js
@@ -1,16 +1,16 @@
 function BeginSessionView() 
 {
 	this.radioButtonSize = TEXT_SIZE;
-	this.radioButtonSetSize = 5 * this.radioButtonSize;
-	this.radioButtons = canvas.drawRadioButtons(0.5 * canvas.width - 0.4 * this.radioButtonSetSize, 
-							100, 
-							"red", 
-							["A", "B", "C"], 
-							[function(){controller.nextState()}, function(){controller.nextState()}, function(){controller.nextState()}], 
+	this.radioButtonSetSize = 3 * this.radioButtonSize;
+	this.hskRadioButtons = canvas.drawRadioButtons(0.5 * (canvas.width - this.radioButtonSetSize), 
+							0.1 * canvas.height, 
+							"blue", 
+							[["HSK 1"], ["HSK 2"]], 
+							[function(){controller.nextState()}, function(){controller.nextState()}], 
 							this.radioButtonSize);
 }
 
 BeginSessionView.prototype.resetRadioButtons = function()
 {
-	this.radioButtons.reset();
+	this.hskRadioButtons.reset();
 }
