e2e9eed095a463ea88aa87ce2fc917e09c482ba9
James Brill <jebssnooker@hotmail.com>
2015-06-30 23:02:13 +0100

    Underline opening menu text

--- static/scripts/begin-session-view.js
+++ static/scripts/begin-session-view.js
@@ -1,22 +1,33 @@
 function BeginSessionView() 
 {
 	this.textSize = 0.25 * TEXT_SIZE;
-	this.topY = 0.1 * canvas.height;
-	this.wordListText = canvas.drawText(0.5 * canvas.width, this.topY, "Word list", "Helvetica", this.textSize);
+	this.underlineThickness = this.textSize / 20;
 	this.radioButtonSize = TEXT_SIZE;
+	this.topY = 0.1 * canvas.height;
 	this.hskRadioButtonSetSize = 3 * this.radioButtonSize;
+	this.wordListText = canvas.drawText(0.5 * canvas.width, this.topY, "Word list", "Helvetica", this.textSize);
+	this.wordListTextUnderline = canvas.drawTextUnderline(0.5 * (canvas.width - 1.1 * this.hskRadioButtonSetSize), 
+														  this.topY + 1.4 * this.textSize, 
+														  1.1 * this.hskRadioButtonSetSize, 
+														  "white", 
+														  this.underlineThickness);
 	this.hskRadioButtons = canvas.drawRadioButtons(0.5 * (canvas.width - this.hskRadioButtonSetSize), 
 							this.topY + 2 * this.textSize, 
 							"blue", 
 							[["HSK 1"], ["HSK 2"]], 
 							[function() { HSK_WORDLIST = 1; }, function() { HSK_WORDLIST = 2; }], 
 							this.radioButtonSize);
+	this.testTypeRadioButtonSetSize = 7 * this.radioButtonSize;
 	this.testTypeText = canvas.drawText(0.5 * canvas.width, 
 										this.topY + 2 * this.textSize + this.radioButtonSize, 
 										"Test type", 
 										"Helvetica", 
 										this.textSize);
-	this.testTypeRadioButtonSetSize = 7 * this.radioButtonSize;
+	this.testTypeTextUnderline = canvas.drawTextUnderline(0.5 * (canvas.width - 1.1 * this.testTypeRadioButtonSetSize), 
+														  this.topY + 3.4 * this.textSize + this.radioButtonSize, 
+														  1.1 * this.testTypeRadioButtonSetSize, 
+														  "white", 
+														  this.underlineThickness);
 	this.testTypeRadioButtons = canvas.drawRadioButtons(0.5 * (canvas.width - this.testTypeRadioButtonSetSize), 
 							this.topY + 4 * this.textSize + this.radioButtonSize, 
 							"blue", 
@@ -39,7 +50,9 @@ BeginSessionView.prototype.displayBeginSessionMenu = function()
 	this.beginSessionButton.box.show();
 	this.beginSessionButton.text.show();
 	this.wordListText.show();
+	this.wordListTextUnderline.show();
 	this.testTypeText.show();
+	this.testTypeTextUnderline.show();
 }
 
 BeginSessionView.prototype.resetButtons = function()
@@ -67,5 +80,7 @@ BeginSessionView.prototype.clear = function()
 	this.beginSessionButton.box.hide();
 	this.beginSessionButton.text.hide();
 	this.wordListText.hide();
+	this.wordListTextUnderline.hide();
 	this.testTypeText.hide();
+	this.testTypeTextUnderline.hide();
 }
