0afad10c28a3019b32182346b5e03adb16a5c9ad
James Brill <jebssnooker@hotmail.com>
2015-11-27 21:00:57 +0000

    Tabs to spaces in controller

--- static/scripts/controller.js
+++ static/scripts/controller.js
@@ -1,40 +1,40 @@
 var controller = function (interactionController, 
-						   flashcardView, 
-						   beginSessionView, 
-						   completeSessionView) {
-	var resetAnswerBoxes = function () {
-		completeSessionView.resetSessionCompleteButtons();
-		flashcardView.resetAnswerBoxes();
-		beginSessionView.resetButtons();
-	}
+                           flashcardView, 
+                           beginSessionView, 
+                           completeSessionView) {
+  var resetAnswerBoxes = function () {
+    completeSessionView.resetSessionCompleteButtons();
+    flashcardView.resetAnswerBoxes();
+    beginSessionView.resetButtons();
+  }
 
-	var hskAnalytics = analytics(hskWordList, FLASHCARD_DISPLAY_MODE);
-	canvas.getBackground().mouseover(function() { resetAnswerBoxes(); });
+  var hskAnalytics = analytics(hskWordList, FLASHCARD_DISPLAY_MODE);
+  canvas.getBackground().mouseover(function() { resetAnswerBoxes(); });
 
-	return {
-		beginSession : function () {
-			hskAnalytics = analytics(hskWordList, FLASHCARD_DISPLAY_MODE);
-			hskAnalytics.reportBeginSession();
-			completeSessionView.clear();
-			beginSessionView.clear();
-			beginSessionView.showCreateNewSessionButton();
-			hskFlashcardController = flashcardController(interactionController, flashcardView);
-			hskFlashcardController.startNewFlashcard();
-		},
-		completeSession : function () {
-			hskAnalytics.reportCompleteSession();
-			flashcardView.clear();
-			hskFlashcardController = null;
-			interactionController.beginAwaitingSessionCompleteKey();
-			completeSessionView.displaySessionCompleteMenu();
-		},
-		newSession : function () {
-			beginSessionView.clear();
-			beginSessionView.hideCreateNewSessionButton();
-			flashcardView.clear();
-			hskFlashcardController = null;
-			completeSessionView.clear();
-			beginSessionView.displayBeginSessionMenu();
-		}
-	};
+  return {
+    beginSession : function () {
+      hskAnalytics = analytics(hskWordList, FLASHCARD_DISPLAY_MODE);
+      hskAnalytics.reportBeginSession();
+      completeSessionView.clear();
+      beginSessionView.clear();
+      beginSessionView.showCreateNewSessionButton();
+      hskFlashcardController = flashcardController(interactionController, flashcardView);
+      hskFlashcardController.startNewFlashcard();
+    },
+    completeSession : function () {
+      hskAnalytics.reportCompleteSession();
+      flashcardView.clear();
+      hskFlashcardController = null;
+      interactionController.beginAwaitingSessionCompleteKey();
+      completeSessionView.displaySessionCompleteMenu();
+    },
+    newSession : function () {
+      beginSessionView.clear();
+      beginSessionView.hideCreateNewSessionButton();
+      flashcardView.clear();
+      hskFlashcardController = null;
+      completeSessionView.clear();
+      beginSessionView.displayBeginSessionMenu();
+    }
+  };
 }
