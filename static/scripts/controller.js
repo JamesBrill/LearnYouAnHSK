155ef94f33a52c1b436bf0e3e75019bdcabbf0a7
James Brill <jebssnooker@hotmail.com>
2015-11-27 20:24:30 +0000

    Remove phase iterator from controller

--- static/scripts/controller.js
+++ static/scripts/controller.js
@@ -8,49 +8,33 @@ var controller = function (interactionController,
 		beginSessionView.resetButtons();
 	}
 
-	var configureForm = function () {
-		beginSessionView.clear();
-		beginSessionView.hideCreateNewSessionButton();
-		flashcardView.clear();
-		hskFlashcardController = null;
-		completeSessionView.clear();
-		beginSessionView.displayBeginSessionMenu();
-	}
-
-	var delegateToFlashcardController = function () {
-		hskAnalytics = analytics(hskWordList, FLASHCARD_DISPLAY_MODE);
-		hskAnalytics.reportBeginSession();
-		completeSessionView.clear();
-		beginSessionView.clear();
-		beginSessionView.showCreateNewSessionButton();
-		hskFlashcardController = flashcardController(interactionController, flashcardView);
-		hskFlashcardController.startNewFlashcard();
-	}
-
-	var completeSession = function () {
-		hskAnalytics.reportCompleteSession();
-		flashcardView.clear();
-		hskFlashcardController = null;
-		interactionController.beginAwaitingSessionCompleteKey();
-		completeSessionView.displaySessionCompleteMenu();
-	}
-
-	var hskPhases = [ "ConfigForm", "DelegateToFlashcardController", "SessionComplete"	];
-	var hskPhaseActions = [ configureForm, delegateToFlashcardController, completeSession ];
-	var phases = phaseIterator(hskPhases, hskPhaseActions);
-
 	var hskAnalytics = analytics(hskWordList, FLASHCARD_DISPLAY_MODE);
 	canvas.getBackground().mouseover(function() { resetAnswerBoxes(); });
 
 	return {
 		beginSession : function () {
-			phases.gotoPhase("DelegateToFlashcardController");
+			hskAnalytics = analytics(hskWordList, FLASHCARD_DISPLAY_MODE);
+			hskAnalytics.reportBeginSession();
+			completeSessionView.clear();
+			beginSessionView.clear();
+			beginSessionView.showCreateNewSessionButton();
+			hskFlashcardController = flashcardController(interactionController, flashcardView);
+			hskFlashcardController.startNewFlashcard();
 		},
 		completeSession : function () {
-			phases.gotoPhase("SessionComplete");
+			hskAnalytics.reportCompleteSession();
+			flashcardView.clear();
+			hskFlashcardController = null;
+			interactionController.beginAwaitingSessionCompleteKey();
+			completeSessionView.displaySessionCompleteMenu();
 		},
 		newSession : function () {
-			phases.gotoPhase("ConfigForm");
+			beginSessionView.clear();
+			beginSessionView.hideCreateNewSessionButton();
+			flashcardView.clear();
+			hskFlashcardController = null;
+			completeSessionView.clear();
+			beginSessionView.displayBeginSessionMenu();
 		}
 	};
 }
