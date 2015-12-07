a1c8411442290d887ab3861ffef59f1eec669450
James Brill <jebssnooker@hotmail.com>
2015-12-07 22:10:52 +0000

    Tabs to spaces

--- static/scripts/views/flashcard-view.js
+++ static/scripts/views/flashcard-view.js
@@ -1,10 +1,10 @@
 function FlashcardView()
 {
-	this.question;
-	this.answer;
-	this.FLASHCARD_TOP = 0.1 * canvas.getHeight();
-	this.discardedCounter = canvas.drawText(0.1 * canvas.getWidth(), 0.4 * canvas.getHeight(), "Discarded: 0", "Helvetica", 0.25 * TEXT_SIZE);
-	this.remainingCounter = canvas.drawText(0.1 * canvas.getWidth(), 0.5 * canvas.getHeight(), "Remaining: 0", "Helvetica", 0.25 * TEXT_SIZE);
+  this.question;
+  this.answer;
+  this.FLASHCARD_TOP = 0.1 * canvas.getHeight();
+  this.discardedCounter = canvas.drawText(0.1 * canvas.getWidth(), 0.4 * canvas.getHeight(), "Discarded: 0", "Helvetica", 0.25 * TEXT_SIZE);
+  this.remainingCounter = canvas.drawText(0.1 * canvas.getWidth(), 0.5 * canvas.getHeight(), "Remaining: 0", "Helvetica", 0.25 * TEXT_SIZE);
 }
 
 FlashcardView.prototype.init = function(displayModeGetter)
@@ -26,155 +26,155 @@ FlashcardView.prototype.init = function(displayModeGetter)
 
 FlashcardView.prototype.displayQuestion = function(memoryWord)
 {
-	var question;
-	switch (this.displayModeGetter())
-	{
-		case FlashcardDisplayMode.CHARACTERS:
-			question = this.drawChinese(memoryWord.characters, this.FLASHCARD_TOP);
-			break;
-		case FlashcardDisplayMode.PINYIN:
-			question = this.drawChinese(memoryWord.pinyin, this.FLASHCARD_TOP);
-			break;
-		case FlashcardDisplayMode.ENGLISH:
-			question = this.drawEnglish(memoryWord.meaning, this.FLASHCARD_TOP);
-			break;
-		case FlashcardDisplayMode.CHARACTERS_AND_PINYIN:
-			question = canvas.drawGroup([this.drawChinese(memoryWord.characters, this.FLASHCARD_TOP),
-							  			 this.drawChinese(memoryWord.pinyin, this.FLASHCARD_TOP + TEXT_SIZE)])
-			break;
-		default:
-			console.log("Invalid flashcard display mode.")
-	}
-	this.question = question;
-	this.showAnswerBoxes();
+  var question;
+  switch (this.displayModeGetter())
+  {
+    case FlashcardDisplayMode.CHARACTERS:
+      question = this.drawChinese(memoryWord.characters, this.FLASHCARD_TOP);
+      break;
+    case FlashcardDisplayMode.PINYIN:
+      question = this.drawChinese(memoryWord.pinyin, this.FLASHCARD_TOP);
+      break;
+    case FlashcardDisplayMode.ENGLISH:
+      question = this.drawEnglish(memoryWord.meaning, this.FLASHCARD_TOP);
+      break;
+    case FlashcardDisplayMode.CHARACTERS_AND_PINYIN:
+      question = canvas.drawGroup([this.drawChinese(memoryWord.characters, this.FLASHCARD_TOP),
+                       this.drawChinese(memoryWord.pinyin, this.FLASHCARD_TOP + TEXT_SIZE)])
+      break;
+    default:
+      console.log("Invalid flashcard display mode.")
+  }
+  this.question = question;
+  this.showAnswerBoxes();
 }
 
 FlashcardView.prototype.initAnswerBoxes = function(height)
 {
-	var easyBoxElements = this.drawAnswerBox(0.3 * canvas.getWidth(),
-											 height,
-											 ["Too easy [Q]"],
-											 "green",
-											 0.15 * this.answerBoxSize,
-											 function() {
-											 	hskFlashcardController.markFlashcardAsEasy();
-											 	hskFlashcardController.startNewFlashcard();
-											 });
-	var showAnswerBoxElements = this.drawAnswerBox(0.5 * canvas.getWidth(),
-												   height,
-												   ["Show answer", "[Space]"],
-												   "gray",
-												   0.05 * this.answerBoxSize,
-												   function() { hskFlashcardController.revealAnswer(); });
-	var hardBoxElements = this.drawAnswerBox(0.7 * canvas.getWidth(),
-											 height,
-											 ["Not easy [W]"],
-											 "red",
-											 0.15 * this.answerBoxSize,
-											 function() { hskFlashcardController.startNewFlashcard(); });
-	this.answerBoxes = [ easyBoxElements, showAnswerBoxElements, hardBoxElements ];
+  var easyBoxElements = this.drawAnswerBox(0.3 * canvas.getWidth(),
+                       height,
+                       ["Too easy [Q]"],
+                       "green",
+                       0.15 * this.answerBoxSize,
+                       function() {
+                         hskFlashcardController.markFlashcardAsEasy();
+                         hskFlashcardController.startNewFlashcard();
+                       });
+  var showAnswerBoxElements = this.drawAnswerBox(0.5 * canvas.getWidth(),
+                           height,
+                           ["Show answer", "[Space]"],
+                           "gray",
+                           0.05 * this.answerBoxSize,
+                           function() { hskFlashcardController.revealAnswer(); });
+  var hardBoxElements = this.drawAnswerBox(0.7 * canvas.getWidth(),
+                       height,
+                       ["Not easy [W]"],
+                       "red",
+                       0.15 * this.answerBoxSize,
+                       function() { hskFlashcardController.startNewFlashcard(); });
+  this.answerBoxes = [ easyBoxElements, showAnswerBoxElements, hardBoxElements ];
 }
 
 FlashcardView.prototype.drawAnswerBox = function(x, y, text, colour, offset, clickHandler)
 {
-	return canvas.drawButton(x, y, text, colour, offset, clickHandler, this.answerBoxSize);
+  return canvas.drawButton(x, y, text, colour, offset, clickHandler, this.answerBoxSize);
 }
 
 FlashcardView.prototype.resetAnswerBoxes = function()
 {
-	for (var i = 0; i < 3; i++)
-	{
-		this.answerBoxes[i].box.attr({ stroke: null });
-	}
+  for (var i = 0; i < 3; i++)
+  {
+    this.answerBoxes[i].box.attr({ stroke: null });
+  }
 }
 
 FlashcardView.prototype.showAnswerBoxes = function()
 {
-	for (var i = 0; i < 3; i++)
-	{
-		this.answerBoxes[i].box.show();
-		this.answerBoxes[i].text.show();
-	}
+  for (var i = 0; i < 3; i++)
+  {
+    this.answerBoxes[i].box.show();
+    this.answerBoxes[i].text.show();
+  }
 }
 
 FlashcardView.prototype.hideAnswerBoxes = function()
 {
-	for (var i = 0; i < 3; i++)
-	{
-		this.answerBoxes[i].box.hide();
-		this.answerBoxes[i].text.hide();
-	}
+  for (var i = 0; i < 3; i++)
+  {
+    this.answerBoxes[i].box.hide();
+    this.answerBoxes[i].text.hide();
+  }
 }
 
 FlashcardView.prototype.showCounters = function()
 {
-	this.discardedCounter.show();
-	this.remainingCounter.show();
+  this.discardedCounter.show();
+  this.remainingCounter.show();
 }
 
 FlashcardView.prototype.hideCounters = function()
 {
-	this.discardedCounter.hide();
-	this.remainingCounter.hide();
+  this.discardedCounter.hide();
+  this.remainingCounter.hide();
 }
 
 FlashcardView.prototype.setCounters = function(discardedCards, remainingCards)
 {
-	this.discardedCounter.text("Discarded: " + discardedCards);
-	this.remainingCounter.text("Remaining: " + remainingCards);
+  this.discardedCounter.text("Discarded: " + discardedCards);
+  this.remainingCounter.text("Remaining: " + remainingCards);
 }
 
 FlashcardView.prototype.displayAnswer = function(memoryWord)
 {
-	var answer;
-	if (this.answer && this.answer.length() != 0) {
-		return;
-	}
-	switch (this.displayModeGetter() )
-	{
-		case FlashcardDisplayMode.CHARACTERS:
-		case FlashcardDisplayMode.PINYIN:
-			answer = this.drawEnglish(memoryWord.meaning, this.FLASHCARD_TOP + 1.2 * TEXT_SIZE);
-			break;
-		case FlashcardDisplayMode.ENGLISH:
-			answer = this.drawChinese(memoryWord.pinyin, this.FLASHCARD_TOP + 0.75 * TEXT_SIZE);
-			break;
-		case FlashcardDisplayMode.CHARACTERS_AND_PINYIN:
-			answer = this.drawEnglish(memoryWord.meaning, this.FLASHCARD_TOP + 2.4 * TEXT_SIZE);
-			break;
-		default:
-			console.log("Invalid flashcard display mode.")
-	}
-	this.answer = answer;
+  var answer;
+  if (this.answer && this.answer.length() != 0) {
+    return;
+  }
+  switch (this.displayModeGetter() )
+  {
+    case FlashcardDisplayMode.CHARACTERS:
+    case FlashcardDisplayMode.PINYIN:
+      answer = this.drawEnglish(memoryWord.meaning, this.FLASHCARD_TOP + 1.2 * TEXT_SIZE);
+      break;
+    case FlashcardDisplayMode.ENGLISH:
+      answer = this.drawChinese(memoryWord.pinyin, this.FLASHCARD_TOP + 0.75 * TEXT_SIZE);
+      break;
+    case FlashcardDisplayMode.CHARACTERS_AND_PINYIN:
+      answer = this.drawEnglish(memoryWord.meaning, this.FLASHCARD_TOP + 2.4 * TEXT_SIZE);
+      break;
+    default:
+      console.log("Invalid flashcard display mode.")
+  }
+  this.answer = answer;
 }
 
 FlashcardView.prototype.drawChinese = function(chinese, height)
 {
-	return canvas.drawText(0.5 * canvas.getWidth(), height, chinese, "SimHei", TEXT_SIZE);
+  return canvas.drawText(0.5 * canvas.getWidth(), height, chinese, "SimHei", TEXT_SIZE);
 }
 
 FlashcardView.prototype.drawEnglish = function(english, height)
 {
-	var englishSize = (english.length > 15) ? 0.5 * TEXT_SIZE : 0.75 * TEXT_SIZE;
-	return canvas.drawText(0.5 * canvas.getWidth(), height, english, "Helvetica", englishSize);
+  var englishSize = (english.length > 15) ? 0.5 * TEXT_SIZE : 0.75 * TEXT_SIZE;
+  return canvas.drawText(0.5 * canvas.getWidth(), height, english, "Helvetica", englishSize);
 }
 
 FlashcardView.prototype.clear = function()
 {
-	this.clearFlashcard();
-	this.hideAnswerBoxes();
-	this.hideCounters();
+  this.clearFlashcard();
+  this.hideAnswerBoxes();
+  this.hideCounters();
 }
 
 FlashcardView.prototype.clearFlashcard = function()
 {
-	if (this.question != undefined)
-	{
-		this.question.clear();
-	}
+  if (this.question != undefined)
+  {
+    this.question.clear();
+  }
 
-	if (this.answer != undefined)
-	{
-		this.answer.clear();
-	}
+  if (this.answer != undefined)
+  {
+    this.answer.clear();
+  }
 }
