46e7c581c43dcfcccc386b60a0300fd4937538e7
James Brill <jebssnooker@hotmail.com>
2015-12-08 00:40:40 +0000

    Encapsulate Begin Session view

--- static/scripts/views/begin-session-view.js
+++ static/scripts/views/begin-session-view.js
@@ -1,45 +1,63 @@
-var createBeginSessionView = function (displayModeSetter)
-{
-  this.textSize = 0.25 * TEXT_SIZE;
-  this.underlineThickness = this.textSize / 20;
-  this.radioButtonSize = TEXT_SIZE;
-  this.topY = 0.1 * canvas.getHeight();
-}
+var createBeginSessionView = function (displayModeSetter) {
+  var textSize = 0.25 * TEXT_SIZE;
+  var underlineThickness = textSize / 20;
+  var radioButtonSize = TEXT_SIZE;
+  var topY = 0.1 * canvas.getHeight();
 
-BeginSessionView.prototype.init = function ()
-{
-  this.createNewSessionButton = this.drawCreateNewSessionButton();
-  this.hskRadioButtonSetSize = 3 * this.radioButtonSize;
-  this.wordListText = canvas.drawText(0.5 * canvas.getWidth(), this.topY, "Word lists", "Helvetica", this.textSize);
-  this.wordListTextUnderline = canvas.drawTextUnderline(0.5 * (canvas.getWidth() - 1.1 * this.hskRadioButtonSetSize),
-                              this.topY + 1.4 * this.textSize,
-                              1.1 * this.hskRadioButtonSetSize,
+  var clear = function () {
+    hskRadioButtons.hide();
+    testTypeRadioButtons.hide();
+    beginSessionButton.box.hide();
+    beginSessionButton.text.hide();
+    wordListText.hide();
+    wordListTextUnderline.hide();
+    testTypeText.hide();
+    testTypeTextUnderline.hide();
+  }
+
+  var hideCreateNewSessionButton = function () {
+    createNewSessionButton.box.hide();
+    createNewSessionButton.text.hide();
+  }
+
+  var createNewSessionButton = canvas.drawButton(0.06 * canvas.getWidth(),
+               0.02 * canvas.getHeight(),
+               ["Create", "new session"],
+               "green",
+               0.05 * radioButtonSize,
+               function() { controller.newSession(); },
+               radioButtonSize);
+  var hskRadioButtonSetSize = 3 * radioButtonSize;
+  var wordListText = canvas.drawText(0.5 * canvas.getWidth(), topY, "Word lists", "Helvetica", textSize);
+  var wordListTextUnderline = canvas.drawTextUnderline(0.5 * (canvas.getWidth() - 1.1 * hskRadioButtonSetSize),
+                              topY + 1.4 * textSize,
+                              1.1 * hskRadioButtonSetSize,
                               "white",
-                              this.underlineThickness);
-  this.hskRadioButtons = canvas.drawRadioButtons(true,
-                           0.5 * (canvas.getWidth() - this.hskRadioButtonSetSize),
-                           this.topY + 2 * this.textSize,
+                              underlineThickness);
+  var hskRadioButtons = canvas.drawRadioButtons(true,
+                           0.5 * (canvas.getWidth() - hskRadioButtonSetSize),
+                           topY + 2 * textSize,
                            "blue",
                            [["HSK 1"], ["HSK 2"]],
                            [
                                function() { hskWordList.toggleHskVersion(1); },
                                function() { hskWordList.toggleHskVersion(2); }
                            ],
-                           this.radioButtonSize);
-  this.testTypeRadioButtonSetSize = 7 * this.radioButtonSize;
-  this.testTypeText = canvas.drawText(0.5 * canvas.getWidth(),
-                    this.topY + 2 * this.textSize + this.radioButtonSize,
+                           radioButtonSize);
+  var testTypeRadioButtonSetSize = 7 * radioButtonSize;
+  var testTypeText = canvas.drawText(0.5 * canvas.getWidth(),
+                    topY + 2 * textSize + radioButtonSize,
                     "Test type",
                     "Helvetica",
-                    this.textSize);
-  this.testTypeTextUnderline = canvas.drawTextUnderline(0.5 * (canvas.getWidth() - 1.1 * this.testTypeRadioButtonSetSize),
-                              this.topY + 3.4 * this.textSize + this.radioButtonSize,
-                              1.1 * this.testTypeRadioButtonSetSize,
+                    textSize);
+  var testTypeTextUnderline = canvas.drawTextUnderline(0.5 * (canvas.getWidth() - 1.1 * testTypeRadioButtonSetSize),
+                              topY + 3.4 * textSize + radioButtonSize,
+                              1.1 * testTypeRadioButtonSetSize,
                               "white",
-                              this.underlineThickness);
-  this.testTypeRadioButtons = canvas.drawRadioButtons(false,
-                            0.5 * (canvas.getWidth() - this.testTypeRadioButtonSetSize),
-                            this.topY + 4 * this.textSize + this.radioButtonSize,
+                              underlineThickness);
+  var testTypeRadioButtons = canvas.drawRadioButtons(false,
+                            0.5 * (canvas.getWidth() - testTypeRadioButtonSetSize),
+                            topY + 4 * textSize + radioButtonSize,
                             "blue",
                             [["Characters +", "Pinyin"], ["Characters"], ["Pinyin"], ["English", "Translation"]],
                             [
@@ -48,74 +66,39 @@ BeginSessionView.prototype.init = function ()
                               function() { displayModeSetter(FlashcardDisplayMode.PINYIN); },
                               function() { displayModeSetter(FlashcardDisplayMode.ENGLISH); }
                             ],
-                            this.radioButtonSize);
-  this.beginSessionButton = this.drawBeginSessionButton();
-  this.clear();
-  this.hideCreateNewSessionButton();
-}
-
-BeginSessionView.prototype.displayBeginSessionMenu = function()
-{
-  this.hskRadioButtons.show();
-  this.testTypeRadioButtons.show();
-  this.beginSessionButton.box.show();
-  this.beginSessionButton.text.show();
-  this.wordListText.show();
-  this.wordListTextUnderline.show();
-  this.testTypeText.show();
-  this.testTypeTextUnderline.show();
-}
-
-BeginSessionView.prototype.resetButtons = function()
-{
-  this.hskRadioButtons.reset();
-  this.testTypeRadioButtons.reset();
-  this.createNewSessionButton.box.attr({ stroke: null });
-  this.beginSessionButton.box.attr({ stroke: null });
-}
-
-BeginSessionView.prototype.drawCreateNewSessionButton = function()
-{
-  return canvas.drawButton(0.06 * canvas.getWidth(),
-               0.02 * canvas.getHeight(),
-               ["Create", "new session"],
-               "green",
-               0.05 * this.radioButtonSize,
-               function() { controller.newSession(); },
-               this.radioButtonSize);
-}
-
-BeginSessionView.prototype.drawBeginSessionButton = function()
-{
-  return canvas.drawButton(0.5 * canvas.getWidth(),
-               0.1 * canvas.getHeight() + 3 * this.radioButtonSize,
+                            radioButtonSize);
+  var beginSessionButton = canvas.drawButton(0.5 * canvas.getWidth(),
+               0.1 * canvas.getHeight() + 3 * radioButtonSize,
                ["Begin session"],
                "green",
-               0.15 * this.radioButtonSize,
+               0.15 * radioButtonSize,
                function() { controller.beginSession(); },
-               this.radioButtonSize);
-}
+               radioButtonSize);
+  clear();
+  hideCreateNewSessionButton();
 
-BeginSessionView.prototype.clear = function()
-{
-  this.hskRadioButtons.hide();
-  this.testTypeRadioButtons.hide();
-  this.beginSessionButton.box.hide();
-  this.beginSessionButton.text.hide();
-  this.wordListText.hide();
-  this.wordListTextUnderline.hide();
-  this.testTypeText.hide();
-  this.testTypeTextUnderline.hide();
-}
-
-BeginSessionView.prototype.showCreateNewSessionButton = function()
-{
-  this.createNewSessionButton.box.show();
-  this.createNewSessionButton.text.show();
-}
-
-BeginSessionView.prototype.hideCreateNewSessionButton = function()
-{
-  this.createNewSessionButton.box.hide();
-  this.createNewSessionButton.text.hide();
+  return {
+    displayBeginSessionMenu : function () {
+      hskRadioButtons.show();
+      testTypeRadioButtons.show();
+      beginSessionButton.box.show();
+      beginSessionButton.text.show();
+      wordListText.show();
+      wordListTextUnderline.show();
+      testTypeText.show();
+      testTypeTextUnderline.show();
+    },
+    resetButtons : function () {
+      hskRadioButtons.reset();
+      testTypeRadioButtons.reset();
+      createNewSessionButton.box.attr({ stroke: null });
+      beginSessionButton.box.attr({ stroke: null });
+    },
+    clear : clear,
+    showCreateNewSessionButton : function () {
+      createNewSessionButton.box.show();
+      createNewSessionButton.text.show();
+    },
+    hideCreateNewSessionButton : hideCreateNewSessionButton
+  };
 }
