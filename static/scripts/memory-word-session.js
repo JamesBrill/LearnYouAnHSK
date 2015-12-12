264fdea5415f0f7b8f82f31c89bfadef04c1d807
James Brill <jebssnooker@hotmail.com>
2015-12-12 12:25:50 +0000

    memory-word-session.js: tabs to spaces

--- static/scripts/memory-word-session.js
+++ static/scripts/memory-word-session.js
@@ -1,6 +1,6 @@
 var createMemoryWordSession = function () {
-	var currentWord;
-	var easyWords = [];
+  var currentWord;
+  var easyWords = [];
   var hardWords = [];
 
   var updateMemoryWords = function () {
@@ -12,39 +12,39 @@ var createMemoryWordSession = function () {
   }
 
   var setHardWord = function () {
-  	var index;
-  	var currentWordHardListIndex = $.inArray(currentWord, hardWords);
-  	if (currentWordHardListIndex != -1 && hardWords.length > 1) {
-  		var copyList = hardWords.slice(0);
-  		copyList.splice(currentWordHardListIndex, 1);
-  		var copyIndex = Math.floor(Math.random() * copyList.length);
-  		var cardChosenFromCopyList = copyList[copyIndex];
-  		index = $.inArray(cardChosenFromCopyList, hardWords);
-  	}
-  	else {
-  		index = Math.floor(Math.random() * hardWords.length);
-  	}
-  	currentWord = hardWords[index];
+    var index;
+    var currentWordHardListIndex = $.inArray(currentWord, hardWords);
+    if (currentWordHardListIndex != -1 && hardWords.length > 1) {
+      var copyList = hardWords.slice(0);
+      copyList.splice(currentWordHardListIndex, 1);
+      var copyIndex = Math.floor(Math.random() * copyList.length);
+      var cardChosenFromCopyList = copyList[copyIndex];
+      index = $.inArray(cardChosenFromCopyList, hardWords);
+    }
+    else {
+      index = Math.floor(Math.random() * hardWords.length);
+    }
+    currentWord = hardWords[index];
   }
 
   return {
     initWords : function () {
       return hskWordList.getWordList().then(function (words) {
         hardWords = words;
-      	updateMemoryWords();
+        updateMemoryWords();
       });
     },
     updateMemoryWords : updateMemoryWords,
     markCurrentWordAsEasy : function () {
-    	hardWords.splice($.inArray(currentWord, hardWords), 1);
-    	console.log(currentWord.meaning + " moved from HARD to EASY");
-    	easyWords.push(currentWord);
+      hardWords.splice($.inArray(currentWord, hardWords), 1);
+      console.log(currentWord.meaning + " moved from HARD to EASY");
+      easyWords.push(currentWord);
     },
     numberOfDiscardedCards : function () {
-    	return easyWords.length;
+      return easyWords.length;
     },
     numberOfRemainingCards : function () {
-    	return hardWords.length;
+      return hardWords.length;
     },
     getCurrentWord : function () {
       return currentWord;
