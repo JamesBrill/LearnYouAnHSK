44ed0e4ee79645e91d0c7e5b9acac18c38a24fbe
James Brill <jebssnooker@hotmail.com>
2015-12-12 13:06:05 +0000

    Move flashcard display mode to enums folder

--- static/scripts/flashcard-display-mode.js
+++
@@ -1,6 +0,0 @@
-function FlashcardDisplayMode() {}
-
-FlashcardDisplayMode.CHARACTERS = 0;
-FlashcardDisplayMode.PINYIN = 1;
-FlashcardDisplayMode.ENGLISH = 2;
-FlashcardDisplayMode.CHARACTERS_AND_PINYIN = 3;
